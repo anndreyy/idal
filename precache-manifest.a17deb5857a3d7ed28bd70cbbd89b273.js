beaac3c3f884cc3b367dffe876caa22a77cb3e2d
Anndrey Francys <anndreyy@live.com>
2019-05-23 01:18:10 -0300

    First Commit

---
+++ precache-manifest.a17deb5857a3d7ed28bd70cbbd89b273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/idal/robots.txt"
+  },
+  {
+    "revision": "1a326757b895e5332768",
+    "url": "/idal/js/chunk-vendors.a991b04d.js"
+  },
+  {
+    "revision": "eaee91297ac4850cfd29",
+    "url": "/idal/js/app.abd4c47f.js"
+  },
+  {
+    "revision": "46d922eafb1f1b494c75",
+    "url": "/idal/js/about.11ac6d99.js"
+  },
+  {
+    "revision": "1ec3b15afeb6c762b493504a1e2c4fc6",
+    "url": "/idal/index.html"
+  },
+  {
+    "revision": "1a326757b895e5332768",
+    "url": "/idal/css/chunk-vendors.4d9c9b27.css"
+  },
+  {
+    "revision": "eaee91297ac4850cfd29",
+    "url": "/idal/css/app.a1ddb2fd.css"
+  }
+];
